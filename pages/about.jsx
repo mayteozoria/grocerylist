ef0eda016bc8d99bdcf2a81fb3652650eaa846c1
Mayte Ozoria <mayteozoria@gmail.com>
2023-11-29 12:39:23 -0500

    Created About page with login credintial information

---
+++ pages/about.jsx
@@ -0,0 +1,40 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import bag from '../public/bag.png'
+import { IoIosArrowRoundBack } from 'react-icons/io'
+
+export default function About() {
+  return (
+    <>
+      <nav>
+        <Link
+          className="font-rocknRollOne font-bold text-3xl text-left"
+          href="/"
+        >
+          <h2>Home</h2>
+          <IoIosArrowRoundBack />
+        </Link>
+      </nav>
+
+      <body>
+        <div className=" py-0 flex flex-row text-2xl">
+          <Image
+            src={bag}
+            // layout="responsive"
+            height={700}
+            width={700}
+            quality={100}
+            alt="Grocery bag "
+          />
+
+          <p>
+            Shopping list app with MongoDB and NextAuth. Add and Delete items to
+            your list. Use Google Auth or Credentials.
+            <p className="font-bold">Username: "fridge" </p>
+            <p className="font-bold">Password: "list" </p>
+          </p>
+        </div>
+      </body>
+    </>
+  )
+}
